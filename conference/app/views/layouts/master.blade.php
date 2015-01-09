dad418fe8e6226dd5b82306d293bf8c37dcf698b
kisPocok <david.schneidhoffer@gmail.com>
2015-01-09 22:51:29 +0100

    Title update

--- conference/app/views/layouts/master.blade.php
+++ conference/app/views/layouts/master.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="hu">
 <head>
-	<title>local.dev</title>
+	<title>Conference!</title>
 	@include('includes.htmlhead')
 	<link rel="stylesheet" href="/app/css/site.css?v=<?= $buildVersion ?>" />
 	<link rel="stylesheet" href="/packages/bootstrap/dist/css/bootstrap.min.css?v=<?= $buildVersion ?>" />
