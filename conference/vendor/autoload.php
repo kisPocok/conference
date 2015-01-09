ee0d3143b7261c42a5372134884ef31d37bfc385
kisPocok <david.schneidhoffer@gmail.com>
2015-01-09 21:15:28 +0100

    Update Readme

--- conference/vendor/autoload.php
+++ conference/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit828017c4f238205bc07cc262930efa42::getLoader();
+return ComposerAutoloaderInitd37e78bc56b1552da2217744bc716b64::getLoader();
